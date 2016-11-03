e73eac33d1d1bb260b22e898c271f61b93e41086
stevemu <steve.mu.dev@gmail.com>
2016-11-03 16:15:43 -0400

    refactoring add product

--- src/app/components/dashboard/AddProduct.js
+++ src/app/components/dashboard/AddProduct.js
@@ -1,24 +1,44 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+
+// function FieldGroup({ id, label, help, ...props }) {
+//   return (
+//     <FormGroup controlId={id}>
+//       <ControlLabel>{label}</ControlLabel>
+//       <FormControl {...props} />
+//       {help && <HelpBlock>{help}</HelpBlock>}
+//     </FormGroup>
+//   );
+// }
 
 class AddProduct extends Component {
   render() {
     return (
-      <div className="">
-        <h1>Add Product</h1>
-
-        <h2>Basic Info</h2>
-        <div className="input-group">
-          <span className="input-group-addon" id="basic-addon1">Model: </span>
-          <input type="text" className="form-control" placeholder="Model" aria-describedby="basic-addon1" />
-        </div>
-        <div className="input-group">
-          <span className="input-group-addon" id="basic-addon1">Price: </span>
-          <input type="text" className="form-control" placeholder="Price" aria-describedby="basic-addon1" />
-        </div>
-
-        <h2>Picture</h2>
-
-      </div>
+      <form action="">
+        <h2>Add Product</h2>
+        {/*<FieldGroup*/}
+          {/*id="formControlsText"*/}
+          {/*type="text"*/}
+          {/*label="Text"*/}
+          {/*placeholder="Enter text"*/}
+        {/*/>*/}
+      </form>
+      // <div className="">
+      //   <h1>Add Product</h1>
+      //
+      //   <div className="input-group">
+      //     <span className="input-group-addon" id="basic-addon1">Model: </span>
+      //     <input type="text" className="form-control" placeholder="Model" aria-describedby="basic-addon1" />
+      //   </div>
+      //   <div className="input-group">
+      //     <span className="input-group-addon" id="basic-addon1">Price: </span>
+      //     <input type="text" className="form-control" placeholder="Price" aria-describedby="basic-addon1" />
+      //   </div>
+      //
+      //   <h2>Picture</h2>
+      //   <Button bsStyle="primary" bsSize="large" block>Submit</Button>
+      //
+      // </div>
     );
   }
 }
--- src/server/db/dbUtil.js
+++ src/server/db/dbUtil.js
@@ -6,6 +6,7 @@ var db = low('db.json');
 db._.mixin(require('underscore-db')); // for getById
 db.defaults({ products: [], orders: []}).value(); // define default schema
 
+
 // get products
 var getProducts = () => {
   const products = db.get('products').value();
