ac21d018f2171b9525e684d3c70b8ad2c2fc5ce8
stevemu <steve.mu.dev@gmail.com>
2016-11-06 01:24:12 -0400

    refactor with app.params

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -3,14 +3,14 @@
 import path from 'path';
 var express = require('express');
 var router = express.Router();
-var _ = require('underscore');
 const uuid = require('uuid');
 
 require('../db/seed'); // seed the db for sample data
 
+const db = require('../db/dbUtil').db;
 
-const dbUtil = require('../db/dbUtil');
-const db = dbUtil.db;
+
+//CRUD API for products, orders, tradeShows
 
 router.param('collection', (req, res, next, collection) => {
   const arr = db.get(collection);
@@ -32,115 +32,23 @@ router.get('/:collection/:id', (req, res) => {
   res.json(req.single.value());
 });
 
-//
-// // products
-//
-// // get all products
-// router.get('/products', function(req, res) {
-//   var products = dbUtil.getProducts();
-//   res.json(products);
-// });
-//
-// // get one product
-// router.get('/products/:id', function(req, res) {
-//   const product = dbUtil.getProductById(req.params.id);
-//   res.json(product);
-// });
-//
-// // post a new product
-// router.post('/products/', function(req, res) {
-//   var product = req.body;
-//   product.id = uuid();
-//   dbUtil.addProduct(product);
-//   res.json({status: "success", id: product.id});
-// });
-//
-// // update a product
-// router.put('/products/:id', function(req, res) {
-//   var product = req.body;
-//   var id = req.params.id;
-//   dbUtil.updateProduct(id, product);
-//   res.json({status: "success"});
-// });
-//
-// // delete a product
-// router.delete('/products/:id', function(req, res) {
-//   var id = req.params.id;
-//   dbUtil.deleteProduct(id);
-//   res.json({status: "success"});
-// });
-//
-//
-//
-// // orders
-//
-// router.get('/orders', function(req, res) {
-//   var orders = dbUtil.getOrders();
-//   res.json(orders);
-// });
-//
-// router.get('/orders/:id', function(req, res) {
-//   const order = dbUtil.getOrderById(req.params.id);
-//   res.json(order);
-// });
-//
-// router.post('/orders/', function(req, res) {
-//   var order = req.body;
-//   order.id = uuid();
-//   dbUtil.addProduct(order);
-//   res.json({status: "success", id: order.id});
-// });
-//
-// router.put('/orders/:id', function(req, res) {
-//   var order = req.body;
-//   var id = req.params.id;
-//   dbUtil.updateOrder(id, order);
-//   res.json({status: "success"});
-// });
-//
-// router.delete('/orders/:id', function(req, res) {
-//   var id = req.params.id;
-//   dbUtil.deleteOrder(id);
-//   res.json({status: "success"});
-// });
-//
-//
-// // trade shows
-//
-// // get all
-// router.get('/trade-shows', function(req, res) {
-//   var objects = dbUtil.getOrders();
-//   res.json(objects);
-// });
-//
-// // get one
-// router.get('/trade-shows/:id', function(req, res) {
-//   const show = dbUtil.getTradeShowById(req.params.id);
-//   res.json(show);
-// });
-//
-// // post a new
-// router.post('/trade-shows/', function(req, res) {
-//   var show = req.body;
-//   show.id = uuid();
-//   dbUtil.addTradeShow(show);
-//   res.json({status: "success", id: show.id});
-// });
-//
-// // update
-// router.put('/trade-shows/:id', function(req, res) {
-//   var show = req.body;
-//   var id = req.params.id;
-//   dbUtil.updateTradeShow(id, show);
-//   res.json({status: "success"});
-// });
-//
-// // delete
-// router.delete('/trade-shows/:id', function(req, res) {
-//   var id = req.params.id;
-//   dbUtil.deleteTradeShow(id);
-//   res.json({status: "success"});
-// });
+router.post('/:collection', (req, res) => {
+  var obj = req.body;
+  obj.id = uuid();
+  req.collection.push(obj).value();
+  res.json({status: "success", id: obj.id});
+});
+
+router.put('/:collection/:id', (req, res) => {
+  req.collection.getById(req.params.id).assign(req.body).value();
+  res.json({status: "success"});
+});
+
+router.delete('/:collection/:id', (req, res) => {
+  req.collection.remove({id: req.params.id}).value();
+  res.json({status: "success"});
+});
+
 
 
 // image upload
--- src/server/db/dbUtil.js
+++ src/server/db/dbUtil.js
@@ -4,77 +4,10 @@ var low = require('lowdb');
 var db = low('db.json');
 
 db._.mixin(require('underscore-db')); // for getById
-db.defaults({ products: [], orders: [], tradeShows: []}).value(); // define default schema
+db.defaults({
+  products: [],
+  orders: [],
+  tradeShows: []
+}).value(); // define default schema
 
 module.exports.db = db;
-
-// products
-module.exports.getProducts = () => {
-  const products = db.get('products').value();
-  return products;
-};
-
-module.exports.getProductById = (id) => {
-  const product = db.get('products').getById(id).value();
-  return product;
-};
-
-module.exports.addProduct = (product) => {
-  db.get('products').push(product).value();
-};
-
-module.exports.updateProduct = (id, product) => {
-  db.get('products').getById(id).assign(product).value();
-};
-
-module.exports.deleteProduct = (id) => {
-  db.get('products').remove({id: id}).value();
-};
-
-// orders
-
-module.exports.getOrders = () => {
-  const orders = db.get('orders').value();
-  return orders;
-};
-
-module.exports.getOrderById = (id) => {
-  const order = db.get('orders').getById(id).value();
-  return order;
-};
-
-module.exports.addOrder = (order) => {
-  db.get('orders').push(order).value();
-};
-
-module.exports.updateOrder = (id, order) => {
-  db.get('orders').getById(id).assign(order).value();
-};
-
-module.exports.deleteOrder = (id) => {
-  db.get('orders').remove({id: id}).value();
-};
-
-// trade shows
-
-module.exports.getTradeShows = () => {
-  const tradeShows = db.get('tradeShows').value();
-  return tradeShows;
-};
-
-module.exports.getTradeShowById = (id) => {
-  const show = db.get('tradeShows').getById(id).value();
-  return show;
-};
-
-module.exports.addTradeShow = (order) => {
-  db.get('tradeShows').push(order).value();
-};
-
-module.exports.updateTradeShow = (id, order) => {
-  db.get('tradeShows').getById(id).assign(order).value();
-};
-
-module.exports.deleteTradeShow = (id) => {
-  db.get('tradeShows').remove({id: id}).value();
-};
